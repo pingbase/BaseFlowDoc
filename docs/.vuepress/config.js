371cc37a0d7cfad8611663bc2b5b3105a6d83f71
Ming <yangming@pingbase.cn>
2023-05-10 12:34:33 +0800

    init file staructs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'BaseFlow',
+  description: 'Welcome to BaseFlowDoc!',
+  themeConfig: {
+    nav: [
+        { text: "首页", link: "/" },
+        { text: "导航1", link: "/" },
+        { text: "导航2", link: "/" }
+    ]
+  }
+}
