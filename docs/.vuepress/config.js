70963ebc8a5693077f2894c1c093535dca683e18
Ming <yangming@pingbase.cn>
2023-05-10 17:18:45 +0800

    init doc structs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,38 @@
 module.exports = {
   title: 'BaseFlow',
   description: 'Welcome to BaseFlowDoc!',
+  theme: 'reco',
   themeConfig: {
+    subSidebar: 'auto',
     nav: [
         { text: "首页", link: "/" },
-        { text: "导航1", link: "/" },
-        { text: "导航2", link: "/" }
+        { text: 'Github', link: 'https://github.com/pingbase/BaseFlowDoc' },
+    ],
+    sidebar: [
+      {
+          title: 'Welcome',
+          path: '/',
+          collapsable: false
+      },
+      {
+        title: '云应用',
+        path: '/app/app',
+        collapsable: false,
+        children: [
+            { title: "我的应用", path: "/app/app" },
+            { title: "应用模板", path: "/app/template" }
+        ]
+    },
+      {
+        title: "关于我们",
+        path: '/about/about',
+        collapsable: false,
+      }
     ]
-  }
+  },
+  locales: {
+    '/': {
+      lang: 'zh-CN'
+    }
+  },
 }
